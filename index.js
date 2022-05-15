b53dc2450a3acf8953156f1cbdfd419b298eb0db
AnNaTe07 <102766959+AnNaTe07@users.noreply.github.com>
2022-05-15 16:45:16 -0300

    cambios de navegador, pie de pagina y contacto

---
+++ index.js
@@ -0,0 +1,7 @@
+const menuNavegador=document.querySelector(".menu-navegador")
+const nav-ul=document.querySelector(".nav-ul")
+
+
+nav-ul.addEventListener("click",()=>{
+    menu_v.classList.toggle("menu_v");
+})
