6649dc34a65f8aa61d88617f669f23952211f3b6
AnNaTe07 <102766959+AnNaTe07@users.noreply.github.com>
2022-05-21 23:19:23 -0300

    carrousel

--- index.js
+++ index.js
@@ -1,7 +1,96 @@
-const menuNavegador=document.querySelector(".menu-navegador")
-const nav-ul=document.querySelector(".nav-ul")
+window.onload = function () {
+    // Variables
+    const IMAGENES = [
+        'carrusel/1930.jpg',
+        'carrusel/1934.jpg',
+        'carrusel/1958.jpg',
+        'carrusel/1962.jpg',
+        'carrusel/1966.jpg',
+        'carrusel/1974.jpg',
+        'carrusel/1978.jpg',
+        'carrusel/1982.jpg',
+        'carrusel/1986.jpg',
+        'carrusel/1990.jpg',
+        'carrusel/2002.jpg',
+        'carrusel/2006.jpg',
+        'carrusel/2010.jpg',
+        'carrusel/2014.jpg',
+        'carrusel/2018.jpg',
+        'carrusel/2022.jpg'
 
+    ];
+    const TIEMPO_MILISEG = 3000;
+    let posicionActual = 0;
+    let $botonRetroceder = document.querySelector('#anterior');
+    let $botonAvanzar = document.querySelector('#siguiente');
+    let $imagen = document.querySelector('#imagen');
+    let $botonPlay = document.querySelector('#play');
+    let $botonStop = document.querySelector('#stop');
+    let intervalo;
 
-nav-ul.addEventListener("click",()=>{
-    menu_v.classList.toggle("menu_v");
-})
+    // Funciones
+
+    /**
+     * Funcion que cambia la foto en la siguiente posicion
+     */
+    function pasarFoto() {
+        if(posicionActual >= IMAGENES.length - 1) {
+            posicionActual = 0;
+        } else {
+            posicionActual++;
+        }
+        renderizarImagen();
+    }
+
+    /**
+     * Funcion que cambia la foto en la anterior posicion
+     */
+    function retrocederFoto() {
+        if(posicionActual <= 0) {
+            posicionActual = IMAGENES.length - 1;
+        } else {
+            posicionActual--;
+        }
+        renderizarImagen();
+    }
+
+    /**
+     * Funcion que actualiza la imagen de imagen dependiendo de posicionActual
+     */
+    function renderizarImagen () {
+        $imagen.style.backgroundImage = `url(${IMAGENES[posicionActual]})`;
+    }
+
+    /**
+     * Activa el autoplay de la imagen
+     */
+    function playIntervalo() {
+        intervalo = setInterval(pasarFoto, TIEMPO_MILISEG);
+        // Desactivamos los botones de control
+        $botonAvanzar.setAttribute('disabled', true);
+        $botonRetroceder.setAttribute('disabled', true);
+        $botonPlay.setAttribute('disabled', true);
+        $botonStop.removeAttribute('disabled');
+
+    }
+
+    /**
+     * Para el autoplay de la imagen
+     */
+    function stopIntervalo() {
+        clearInterval(intervalo);
+        // Activamos los botones de control
+        $botonAvanzar.removeAttribute('disabled');
+        $botonRetroceder.removeAttribute('disabled');
+        $botonPlay.removeAttribute('disabled');
+        $botonStop.setAttribute('disabled', true);
+    }
+
+    // Eventos
+    $botonAvanzar.addEventListener('click', pasarFoto);
+    $botonRetroceder.addEventListener('click', retrocederFoto);
+    $botonPlay.addEventListener('click', playIntervalo);
+    $botonStop.addEventListener('click', stopIntervalo);
+    // Iniciar
+    renderizarImagen();
+} 
