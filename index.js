8cd2f5f36c615db69dc3587c93f85c0920479b81
AnNaTe07 <102766959+AnNaTe07@users.noreply.github.com>
2022-05-25 11:26:35 -0300

    modificaciones, nav y aside

--- index.js
+++ index.js
@@ -95,3 +95,67 @@ window.onload = function () {
     // Iniciar
     renderizarImagen();
 } 
+
+//VALIDAR FORM
+
+function ver(e){
+    console.log(e);
+}
+
+function enviar(e){
+    if(validar()){
+        let nombres=document.getElementById("nombres");
+        let apellido=document.querySelector("#apellido");
+        let dni=document.getElementById("dni");
+        let correo=document.forms.correo;
+        let telefono=document.getElementById("telefono");
+        let p =document.createElement("p");
+        p.innerHTML='${nombres.value} ${apellido.value} ,${dni.value} ,(${correo.value})';
+        
+        document.getElementById("demo").appendChild(p);
+    }
+    return false;
+}
+
+function vaidar(e){/*classlist*/
+    let nombres=document.getElementById("nombres");
+    let apellido=document.querySelector("#apellido");
+    let dni=document.getElementById("dni");
+    let correo=document.forms.correo;
+    let telefono=document.getElementById("telefono");
+    let errores =[];
+
+    let inputs = document.querySelectorAll("input");
+    console.log(inputs);
+    for(i=0;i<inputs.length;i++){
+        inputs[i].style.border="revert";/*style*/
+    }
+
+    if(nombres.value.trim()==""){
+        errores.push("Nombres no puede ser vacio");
+        nombres.style.border="1px solid red";
+    }else if(nombres.value.length>30){
+        errores.push("Nombres no puede exceder 30 caracteres");
+    }
+
+    if(apellido.value==""){
+        errores.push("Apellido no puede ser vacio");
+    }else if(apellido.value.length>20){
+        errores.push("Apellido no puede exceder 20 caracteres");
+    }
+
+    if(dni.value==""){
+        console.log("Dni no puede ser vacio");
+    }else if(dni.value.length>7){
+        errores.push("Nombres no puede exceder 7 caracteres");
+    }
+
+    let er=/^([a-zA-Z0-9_\-\.]+)@([a-zA-Z0-9_\-\.]+)\.([a-zA-Z]{2,5})$ /;
+
+    if(!er.test(correo.value)){
+        errores.push("Debe ser un e-mail válido");
+    }
+
+    console.log(nombre);
+    return errores.length==0;
+}
